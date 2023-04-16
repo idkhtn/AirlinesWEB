322220cfd600ffbac97c04aee6cdd46825b1767c
idkhtn <fjn46@interia.eu>
2023-04-16 06:39:07 +0200

    Add project files.

---
+++ Airlines.Intranet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Airlines.Intranet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
